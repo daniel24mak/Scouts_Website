cdc7cb0387d3d31bcb7c2741f47579de172fa8f6
daniel24mak <danial.makhlouf@gmail.com>
2026-06-14 00:13:01 +0400

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/Scouts_Website/",
   plugins: [react()],
   server: {
     proxy: {
